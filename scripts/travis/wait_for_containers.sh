2d8a0b709e2a6e853dc32b98085af59443c07b44
henriquebonadio <henriquebonadio@hotmail.com>
2017-08-19 21:25:12 -0300

    wait for api

--- scripts/travis/wait_for_containers.sh
+++ scripts/travis/wait_for_containers.sh
@@ -6,6 +6,9 @@ echo "Waiting for ODL to go up"
 MAX_RETRY=30
 SLEEP_TIME=10
 
+ODL_READY=0
+API_READY=0
+
 for i in $(seq 1  ${MAX_RETRY}); do
     
     sleep ${SLEEP_TIME}
@@ -13,9 +16,20 @@ for i in $(seq 1  ${MAX_RETRY}); do
 
     # If the port is open we continue with the script
     if [ "$?" -eq "0" ]; then
-        break;
+        echo "ODL server is ready";
+        ODL_READY=1;
     fi
 
+    API_READY=$(docker exec netapi_app echo $IAMREADY)
+    if [ "$ODL_READY" -eq "1"]
+        echo "API is ready";
+    fi
+    
+    if [ "$ODL_READY" -eq "1" && "$API_READY" -eq "1"]
+        echo "Going on with the tests";
+        break;
+    fi
+    
     # When maximum retries is achieved we exit with an error message
     if [ "$i" -eq "${MAX_RETRY}" ]; then
         echo "Max retry achieved"
@@ -25,4 +39,4 @@ for i in $(seq 1  ${MAX_RETRY}); do
     echo "Retrying ${i}.."
 done
 
-echo "ODL server is up"
+
